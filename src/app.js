a08dfa5fdcbb37167db2cc5339b3ffb0d4eb9306
huseynmv <huseynmv@yahoo.com>
2023-04-03 12:20:34 +0400

    cors

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors({ origin: "*" }));
 mongoose
   .connect(CONNECTION_STRING)
   .then((res) => console.log("Connected to DB"))
@@ -22,7 +23,6 @@ app.use("/api/car", carRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use(cors({ origin: "*" }));
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
     message: err?.message || "Server error",
